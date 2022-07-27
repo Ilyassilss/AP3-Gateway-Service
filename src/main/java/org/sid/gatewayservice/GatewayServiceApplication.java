48c470ff4197559bbd6b04cb6337188b5b77feac
ilyass <elmofid.ilyass@gmail.com>
2022-07-27 11:41:01 +0100

    final update

--- src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -16,17 +16,30 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    //@Bean
-    /*RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes
+            (ReactiveDiscoveryClient rdc , DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
+
+
+    
+    /*@Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
                 .route(r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .build();
     }*/
 
-    @Bean
-    DiscoveryClientRouteDefinitionLocator dynamicRoutes
-            (ReactiveDiscoveryClient rdc , DiscoveryLocatorProperties dlp){
-        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
-    }
+    /*@Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/customers/**").uri("http://localhost:8081/"))
+                .route(r -> r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }*/
+
+
 }
